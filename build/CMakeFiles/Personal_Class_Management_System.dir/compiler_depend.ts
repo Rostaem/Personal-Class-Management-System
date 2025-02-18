f6e5379db29d3b8e490b7fdb1bc46c924a67d875
Hania <freshjuicymangos6@gmail.com>
2025-02-17 19:20:35 -0800

    Remove build folder and add to .gitignore

--- build/CMakeFiles/Personal_Class_Management_System.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for Personal_Class_Management_System.
--- build/CMakeFiles/Personal_Class_Management_System.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Personal_Class_Management_System.
-# This may be replaced when dependencies are built.
