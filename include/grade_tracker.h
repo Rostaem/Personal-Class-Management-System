b81b7ff9d924182eed19b9a33e84e13d14980061
Hania <freshjuicymangos6@gmail.com>
2025-05-02 23:35:50 -0700

    added a new test case for grade_tracker.cpp

--- include/grade_tracker.h
+++ include/grade_tracker.h
@@ -1,12 +1,18 @@
 #ifndef GRADE_TRACKER_H
 #define GRADE_TRACKER_H
 
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <map>
+
+extern std::unordered_map<std::string, std::vector<float>> grades_by_course;
+extern std::map<std::string, std::vector<float>> sorted_grades_by_course;
+
 void manage_grades();
 void record_grade();
 void calculate_grades();
 void display_grades();
-
 void save_grades();
 void load_grades();
-
 #endif // GRADE_TRACKER_H
