db601e1c1d1c356a2f70ecff6a8c1ef5925e4cdc
Hania <freshjuicymangos6@gmail.com>
2025-05-16 23:43:24 -0700

    created a new cpp course file and class

---
+++ src/course_management.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Usuario1 on 5/16/25.
+//
+
+#include "course_management.h"
---
+++ src/course_management.h
@@ -0,0 +1,8 @@
+//
+// Created by Usuario1 on 5/16/25.
+//
+
+#ifndef COURSE_MANAGEMENT_H
+#define COURSE_MANAGEMENT_H
+
+#endif //COURSE_MANAGEMENT_H
